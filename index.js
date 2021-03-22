8666b86889ade8a9547efd63110a37a2b976d1f2
Jon <jbrundagejr@gmail.com>
2021-03-22 08:58:23 -0400

    commit tree 5cf0c3a9b8e293547897ac746ed2888920645f4e

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => renderBooks(json))
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
